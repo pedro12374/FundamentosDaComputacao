d917b232a2d38fabc5bab4ee335289469f98f9b5
Pedro Haerter <pedrohaerter@hotmail.com>
2016-10-02 23:34:14 -0300

    Revert "exercicos de fundametos"
    This reverts commit 5c2304015e9b62e21a84359f9441bf6cb88f6d31.

--- circuferencia.pas
+++
@@ -1,9 +0,0 @@
-program area_circulo;
-var area: Real;
-begin
-	write('Entre com o raio da circuferencia: ');
-	readln(area);
-	//pi = 3.14;
-	area:= pi*(area*area);
-	writeln('A area da circuferencia é de: ',area:7:2,' u c');
-end.
