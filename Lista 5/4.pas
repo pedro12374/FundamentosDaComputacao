3dd7ec9bc5449a27752f8a5c9cab44b7634e146a
Haerter <pedrohaerter@hotmail.com>
2016-12-11 17:45:05 -0200

    Lista 5

--- Lista 5/4.pas
+++ Lista 5/4.pas
@@ -5,9 +5,22 @@ begin
   Writeln('Entre com uma base e um exponente(positivo e inteiro)');
   readln(base,exp);
   res:=1;
-  for i:=1 to exp do
-    begin
-      res:=base*res;
+  if (exp>0)
+    then begin
+      for i:=1 to exp do
+        begin
+          res:=base*res;
+      end;
+    end
+    else if(exp<0)
+      then begin
+            for i:=1 downto exp do
+              begin
+                res:=base*res;
+            end;
+               res:=1/res;
     end;
-    Writeln(base:0:2,' elevado a ', exp,' é ',res:0:2);
-  end.
+
+
+  Writeln(base:0:2,' elevado a ', exp,' é ',res:0:2);
+end.
