619e8c6d754a4210af9190b86522728b68d201fa
Haerter <pedrohaerter@hotmail.com>
2016-12-21 18:22:59 -0200

    Lista 6

--- Lista 1/idade3.pas
+++ Lista 1/idade3.pas
@@ -3,8 +3,8 @@ var idd,mes,anos,dias: integer;
 begin
 	writeln('Qual a sua idade em dias kerido');
 	read(idd);
-	anos:=idd div 365;
-	mes:=(idd - 365*anos) div 30; 
+	anos:=idd / 365;
+	mes:=(idd - 365*anos) / 30;
 	dias:= ((idd - 365*anos) - 30*mes);
 	writeln('Você tem ',anos,' anos,',mes,' meses,',dias,' dias de uma fodendo vida.');
-end. 
+end.
