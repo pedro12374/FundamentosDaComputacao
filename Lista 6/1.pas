ac0e3b81498f0bb441c6f91d3fea8aa2488aa2e0
pedro12374 <pedrohaerter@hotmail.com>
2017-01-08 21:52:43 -0200

    primeiro commit

---
+++ Lista 6/1.pas
@@ -0,0 +1,19 @@
+Program Pos_Neg;
+const fim:= 9;
+var n,i,k,j:integer;
+  vet,vetp,vetn:array[0..fim] of Integer;
+begin
+  k:=0;
+  j:=0;
+  for i:=0 to fim do
+    begin
+      write('Entre com o numero ',i,' de 10: ');
+      readln(n);
+      vet[i]:=n
+      if(n>=0)
+        then j:=j + 1
+        else k:=k + 1;
+    end;
+    for i:=0 to j do
+      vetp
+end.
